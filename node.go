3484e384bd9f5b22069fe5532fc5633f05d161f1
Nick von Pentz <nvonpentz@wesleyan.edu>
2017-08-06 14:11:17 -0600

    move transmission to own file

--- node.go
+++ node.go
@@ -22,17 +22,6 @@ type Node struct {
 }
 
 /*
-A Transmission is simply a message that will be sent throughout the network.  
-It includes the actual message as well as the addresses of nodes who have
-already received the Transmission
-*/
-type Transmission struct {
-    Block Block
-    BeenSent bool
-    Sender string
-}
-
-/*
 Every communication between nodes over the TCP connection will be 
 through sending this communication object.  The ID represents the type:
 
@@ -131,14 +120,6 @@ func (n Node) getConnForAddress(address string) (net.Conn){
     }
     var emptyConn net.Conn
     return emptyConn
-}
-
-func (t *Transmission) updateBeenSent() {
-    t.BeenSent = true
-}
-
-func (t *Transmission) updateSender(address string){
-    t.Sender = address
 }
 
 /*----------------------*
