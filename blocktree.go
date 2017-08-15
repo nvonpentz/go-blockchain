bf148d4a8601f455fe68582a9ed5228f3503e14a
Nick von Pentz <nvonpentz@wesleyan.edu>
2017-08-15 17:38:51 -0600

    scaffold blocktree.go, beginning tests

---
+++ blocktree.go
@@ -0,0 +1,65 @@
+package main 
+
+import(
+	"crypto/sha256"
+	"encoding/binary"
+	"fmt"
+)
+
+type BTNode struct {
+	Height	   uint32
+	Parent     *BTNode
+	ParentHash []byte
+	Data       string
+	Hash 	   []byte
+}
+
+type BlockTree struct {
+	Levels [][]*BTNode
+}
+
+var genesisNode = BTNode{Height: 0, Parent: nil, ParentHash: []byte{0}, Data: "Genesis", Hash: []byte{0}}
+
+func (bt *BlockTree) addBTNodeIfValid(newBTNode *BTNode) {
+	parentHeight         := newBTNode.Height - 1
+	nodesAtLevelOfParent := bt.Levels[parentHeight]
+
+	for _ , oldBTNode := range nodesAtLevelOfParent {
+		if oldBTNode.isValidBTNode(*newBTNode) {
+			// append to parent level
+			bt.Levels[newBTNode.Height] = append(bt.Levels[newBTNode.Height], newBTNode)
+		} else {
+			fmt.Println("No matching node found")
+		}
+	}
+} // should check to see which is now the longest
+
+func (oldBTNode BTNode) isValidBTNode(newBTNode BTNode) bool {
+	heightValid := oldBTNode.Height + 1 == newBTNode.Height
+	parentValid := newBTNode.Parent == &oldBTNode
+	hashValid   := testEqByteSlice(oldBTNode.Hash, newBTNode.ParentHash)
+
+	return heightValid && parentValid && hashValid
+}
+
+func equalBTNodes(b1, b2 BTNode) bool {
+	heightEq     := b1.Height == b2.Height
+	parentHashEq := testEqByteSlice(b1.ParentHash, b2.ParentHash)
+	dataEq       := b1.Data == b2.Data
+	hashEq 		 := testEqByteSlice(b1.Hash, b2.Hash)
+
+	return heightEq && parentHashEq && dataEq && hashEq
+}
+
+func (b *BTNode) calcBTNodeHash() []byte {
+	var height []byte
+	binary.LittleEndian.PutUint32(height, b.Height)
+	data := []byte(b.Data)
+
+	h := sha256.New()
+	h.Write(height)
+	h.Write(data)
+	h.Write(b.ParentHash)
+
+	return h.Sum(nil)
+}
